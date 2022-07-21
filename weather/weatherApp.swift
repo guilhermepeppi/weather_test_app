a066449a3462814f78a95b0ec2a3ecff8003d308
Guilherme Peppi <guilhermepeppi@gmail.com>
2022-07-21 08:51:45 -0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Guilherme Peppi on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
